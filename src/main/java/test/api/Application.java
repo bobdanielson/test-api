ee2b1e84dc68b36d34031f34842ec0da80e94e17
Bob <bob@bobs-air.lan>
2022-06-25 08:46:31 -0500

    initial commit

---
+++ src/main/java/test/api/Application.java
@@ -0,0 +1,9 @@
+package test.api;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
